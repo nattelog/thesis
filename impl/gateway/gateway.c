9d93226037e73d6c258ccf973d053c4df994e580
Natanael Log <natlo809@student.liu.se>
2018-03-14 12:26:30 +0100

    clean comments

--- impl/gateway/gateway.c
+++ impl/gateway/gateway.c
@@ -5,91 +5,17 @@
 #include "state.h"
 
 typedef struct {
-    uv_tcp_t* client;
-    uv_connect_t* connect_req;
-    uv_write_t* write_req;
-    uv_shutdown_t* shutdown_req;
-    char* data;
-} context_t;
-
-typedef struct {
     ssize_t n;
     const uv_buf_t* buf;
 } chunk_context_t;
 
-void check_r(int r, char* msg) {
+void check_r(int r, char* msg)
+{
     if (r != 0) {
         log_error("%s: [%s(%d): %s]\n", msg, uv_err_name((r)), (int) r, uv_strerror((r)));
         exit(1);
     }
 }
-
-/*
-void destroy_context(context_t* context) {
-    free(context->client);
-    free(context->connect_req);
-    free(context->write_req);
-    free(context->shutdown_req);
-    free(context);
-}
-
-void on_close(uv_shutdown_t* req, int status) {
-    check_r(status, "on_connect");
-}
-
-void alloc_cb(uv_handle_t* handle, size_t n, uv_buf_t* buf) {
-    buf->base = malloc(n);
-    buf->len = n;
-}
-
-void read_cb(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
-    int r = 0;
-    context_t* context = client->data;
-
-    if (nread < 0) {
-        if (nread != UV_EOF) {
-            check_r(nread, "read_cb");
-        }
-
-        free(buf->base);
-        context->shutdown_req = malloc(sizeof(uv_shutdown_t));
-
-        r = uv_shutdown(context->shutdown_req, client, on_close);
-        check_r(r, "uv_shutdown");
-    }
-
-    log_info("read data: %s", buf->base);
-}
-
-void on_write(uv_write_t* req, int status) {
-    check_r(status, "on_write");
-
-    log_info("wrote data");
-}
-
-void on_connect(uv_connect_t* req, int status) {
-    check_r(status, "on_connect");
-
-    log_info("Connected %d", status);
-
-    int r = 0;
-    context_t* context = req->data;
-    context->write_req = malloc(sizeof(uv_write_t));
-    context->write_req->data = context;
-    context->data = "{\"method\":\"blabla\", \"args\":[]}\n";
-    int n = strlen(context->data);
-
-    uv_buf_t bufs[] = {
-        { .base = context->data, .len = n }
-    };
-
-    r = uv_read_start((uv_stream_t*) context->client, alloc_cb, read_cb);
-    check_r(r, "uv_read_start");
-
-    r = uv_write(context->write_req, (uv_stream_t*) context->client, bufs, 1, on_write);
-    check_r(r, "uv_write");
-}
-*/
 
 void on_connection(uv_stream_t* server, int status)
 {
@@ -208,28 +134,8 @@ void on_writing(state_t* state, void* payload)
     log_info("on_writing::%s", state->name);
 }
 
-int main() {
-    /*
-    int r = 0;
-    uv_loop_t* loop = uv_default_loop();
-    struct sockaddr_in addr;
-    context_t* context = malloc(sizeof(context_t));
-
-    context->client = malloc(sizeof(uv_tcp_t));
-    context->client->data = context;
-    context->connect_req = malloc(sizeof(uv_connect_t));
-    context->connect_req->data = context;
-
-    r = uv_tcp_init(loop, context->client);
-    check_r(r, "uv_tcp_init");
-
-    r = uv_ip4_addr("0.0.0.0", 5000, &addr);
-    check_r(r, "uv_ip4_addr");
-
-    r = uv_tcp_connect(context->connect_req, context->client, &addr, on_connect);
-    uv_run(loop, UV_RUN_DEFAULT);
-    */
-
+int main()
+{
     const state_initializer_t si[] = {
         { .name = "start", .callback = on_start },
         { .name = "connecting", .callback = on_connecting },
