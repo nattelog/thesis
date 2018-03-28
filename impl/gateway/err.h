a9d175281e4ffba4cd2a107cc6ab6337a82db34c
Natanael Log <natlo809@student.liu.se>
2018-03-28 16:13:47 +0200

    add some error codes

--- impl/gateway/err.h
+++ impl/gateway/err.h
@@ -1,16 +1,20 @@
 #ifndef __ERR_h__
 #define __ERR_h__
 
-#define ENULL 1
-#define EARGC 2
-#define EJSON 3
-#define EPTCL 4
+#define ENULL -1
+#define EARGC -2
+#define EJSON -3
+#define EPTCL -4
+#define ENFND -5
+#define EBNDS -6
 
 #define GW_ERRNO_MAP(XX) \
     XX(ENULL, "null pointer") \
     XX(EARGC, "bad amount of arguments") \
     XX(EJSON, "cannot parse json") \
     XX(EPTCL, "bad json protocol") \
+    XX(ENFND, "not found") \
+    XX(EBNDS, "out of bounds") \
 
 const char* gw_strerror(int err);
 
