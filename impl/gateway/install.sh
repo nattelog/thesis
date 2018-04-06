210d73370c9ee0027db399bb3e1a85cd9f1b97a3
Natanael Log <natlo809@student.liu.se>
2018-04-06 15:08:11 +0200

    add install script to thpool lib

--- impl/gateway/install.sh
+++ impl/gateway/install.sh
@@ -6,6 +6,7 @@ UVCOMMIT="c5afc37e2a8a70d8ab0da8dac10b77ba78c0488c"
 CHECKDIR="check"
 CHECKCOMMIT="65e8c5e36f2841be16c4c96bae7a7bc171ff7d67"
 JSONDIR="json"
+TPDIR="thpool"
 
 if [ ! -d ${UVDIR} ]; then
     echo "Installing libuv..."
@@ -33,12 +34,22 @@ fi;
 
 if [ ! -d ${JSONDIR} ]; then
     echo "Downloading json lib..."
+    mkdir ${JSONDIR}
     curl https://raw.githubusercontent.com/udp/json-parser/master/json.c > \
         ${JSONDIR}/json.c
     curl https://raw.githubusercontent.com/udp/json-parser/master/json.h > \
         ${JSONDIR}/json.h
-    curl https://raw.githubusercontent.com/udp/json-builder/master/json-builder.c \
+    curl https://raw.githubusercontent.com/udp/json-builder/master/json-builder.c > \
         ${JSONDIR}/json-builder.c
-    curl https://raw.githubusercontent.com/udp/json-builder/master/json-builder.h \
+    curl https://raw.githubusercontent.com/udp/json-builder/master/json-builder.h > \
         ${JSONDIR}/json-builder.h
+fi;
+
+if [ ! -d ${TPDIR} ]; then
+    echo "Downloading thread pool lib..."
+    mkdir ${TPDIR}
+    curl https://raw.githubusercontent.com/Pithikos/C-Thread-Pool/master/thpool.h > \
+        ${TPDIR}/thpool.h
+    curl https://raw.githubusercontent.com/Pithikos/C-Thread-Pool/master/thpool.c > \
+        ${TPDIR}/thpool.c
 fi;
